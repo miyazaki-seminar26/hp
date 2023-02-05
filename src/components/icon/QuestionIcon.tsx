a03e6108bbfc425d3ac709baf2d84652f66692b3
Shin.Yamamoto <yamashin0123@outlook.jp>
2023-02-06 05:01:19 +0900

    feat: ✨ QuestionIcon.tsx

---
+++ src/components/icon/QuestionIcon.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react'
+
+export const QuestionIcon: FC = () => {
+  return (
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <g clip-path='url(#clip0_346_452)'>
+        <path
+          d='M11.9999 22.909C18.0248 22.909 22.909 18.0248 22.909 11.9999C22.909 5.97499 18.0248 1.09082 11.9999 1.09082C5.97499 1.09082 1.09082 5.97499 1.09082 11.9999C1.09082 18.0248 5.97499 22.909 11.9999 22.909Z'
+          stroke='#3D3D3D'
+          stroke-width='2'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+        />
+        <path
+          d='M8.82544 8.72723C9.08192 7.99814 9.58815 7.38334 10.2545 6.99174C10.9208 6.60013 11.7042 6.45698 12.466 6.58764C13.2278 6.7183 13.9187 7.11435 14.4164 7.70562C14.9142 8.2969 15.1866 9.04525 15.1854 9.81814C15.1854 12 11.9127 13.0909 11.9127 13.0909'
+          stroke='#3D3D3D'
+          stroke-width='2'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+        />
+        <path
+          d='M12 17.4546H12.0103'
+          stroke='#3D3D3D'
+          stroke-width='2'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+        />
+      </g>
+      <defs>
+        <clipPath id='clip0_346_452'>
+          <rect width='24' height='24' fill='white' />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
