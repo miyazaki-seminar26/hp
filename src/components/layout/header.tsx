f651deec5861feb42871f4f44bf47ff0b7b86618
Shin.Yamamoto <yamashin0123@outlook.jp>
2023-02-05 23:34:32 +0900

    feat(add files): セットアップのファイルなど

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+
+export const AppHeader: FC = () => (
+  <header>
+    <h1>App Header</h1>    
+  </header>
+)
---
+++ src/components/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import { Title } from '@mantine/core'
+
+export const Home: FC = () => (
+  <Title order={1} className='text-lg'>
+    ホーム
+  </Title>
+)
